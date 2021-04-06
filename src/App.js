3f5c534d0a74db924a853eb70b2c83131a018f32
Tahmass Raheem <tahmassraheem@gmail.com>
2021-04-06 12:19:06 +0500

    Task submitted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect, useCallback } from "react";
+import Options from "./options";
+import axios from "axios";
 function App() {
+  const quesurl = "http://localhost:3333/question_list";
+  const optionurl = "http://localhost:3333/options_list";
+  const [question, setquestion] = useState(null);
+  const [options, setoptions] = useState(null);
+  const [selectedradio, setselectedradio] = useState(null);
+  const [score, setscore] = useState(null);
+  let content = null;
+
+  useEffect(() => {
+    axios.get(quesurl).then((response) => {
+      setquestion(response.data);
+    });
+    axios.get(optionurl).then((response) => {
+      setoptions(response.data);
+    });
+  }, []);
+
+  const onValueChange = (e) => {
+    setselectedradio(e.target.value);
+  };
+
+  if (question && options) {
+    content = (
+      <div>
+        {question.map((question) => {
+          return (
+            <div>
+              <h2>{question.title}</h2>
+              <ul>
+                {options.map((options, i) => {
+                  if (options.related_question === question.id)
+                    return (
+                      <div>
+                        <input
+                          type="radio"
+                          value={options.correct_answer_to_question}
+                          onChange={onValueChange}
+                        />
+                        {options.title}
+                      </div>
+                    );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    );
+    if (selectedradio) {
+      setscore(question.points);
+    } else {
+      console.log("wrong choice");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {content}
+      <Options title={score} />
     </div>
   );
 }
---
+++ src/options.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Options = (props) => {
+  return (
+    <div>
+      <h2>Your score is{props.title}</h2>
+    </div>
+  );
+};
+export default Options;
